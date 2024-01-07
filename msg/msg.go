3504daa832d1b2c40a097e3e7009fdeb326c66ba
Joseph Innes <avianpneuma@gmail.com>
2024-01-07 10:42:00 +0100

    Change msg dump to interface

--- msg/msg.go
+++ msg/msg.go
@@ -1,11 +1,11 @@
 package msg
 
 type Msg struct {
-	Timestamp int64  `json:"t"`
-	Env       string `json:"env"`
-	Svc       string `json:"svc"`
-	Fn        string `json:"fn"`
-	Lvl       string `json:"lvl"`
-	Msg       string `json:"msg"`
-	Dump      string `json:"dump,omitempty"`
+	Timestamp int64       `json:"t"`
+	Env       string      `json:"env"`
+	Svc       string      `json:"svc"`
+	Fn        string      `json:"fn"`
+	Lvl       string      `json:"lvl"`
+	Msg       string      `json:"msg"`
+	Dump      interface{} `json:"dump,omitempty"`
 }
